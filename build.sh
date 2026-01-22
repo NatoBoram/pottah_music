1da2b4a402ea1ca0dfc2854f0b408926c65c7e7b
Nato Boram <nato.boram+git@gmail.com>
2026-01-21 20:05:47 -0500

    🚚 Pottah Music.zip

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-rm -f pottah_music.zip
-7z a -bt -mx9 pottah_music.zip assets LICENSE.md pack.mcmeta pack.png README.md
+rm -f 'Pottah Music.zip'
+7z a -bt -mx9 '-xr!*.psd' 'Pottah Music.zip' assets LICENSE.md pack.mcmeta pack.png README.md
